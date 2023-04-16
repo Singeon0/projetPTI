e28ca0317f90eab05defa380e19322deb1922fcc
Singeon0 <Arthur.LORIN@student.umons.ac.be>
2023-04-16 16:16:59 +0200

    Ajout page prof_choix_questions.html
    Transcription de ce que j'ai demandé à chatGPT
    
    Maintenant je dois créer la page prof_choix_questions.html
    Un professeur sur cette page doit pouvoir sélectionner les types de questions qui feront partie de son questionnaire et doit pouvoir personnaliser chaque type de question qu'il sélectionnera et il doit finalement pouvoir sauvegarder le questionnaire.
    
    Le titre de la page sera "Création de questionnaire"
    
    En haut de cette page je veux un input centrée permettant de rentrer le nom du questionnaire.
    
    Dans cette page dois donc s'afficher tous les types de questions disponibles via un label qui sera le “type_question” provenant de la table “type_de_questions” qui occupera 60% de la largeur de l’écran. À gauche de ce label je veux une checkbox permettant de sélectionner la question, si la checkbox est coché la question sera dans le questionnaire, elle occupera 10% de la largeur de l’écran. À droite du label je veux un bouton “Personnaliser” permettant de personnaliser la question et qui occupera 30% de la largeur de l’écran, pour l'instant je ne veux pas affecter de fonctionnalité à ce bouton. Pour les boutons, je veux que tu utilises le style de mon fichier “style2.css”
    
    Tout en bas de l’écran je veux un bouton “Sauvegarder le questionnaire” qui occupe 30% de la largeur de l’écran et qui est rouge et centrée au milieu de l’écran.
    
    Il faut dans cette page que tu puisses récupérer “idProf” provenant de la page “prof.htlm”
    
    Lorsque le professeur cliquera sur le bouton ”Sauvegarder le questionnaire”, en fonction des types de questions qui seront sélectionnés via la checkbox, je veux que tu m’ajoute dans la table “questionnaire” un nouveau questionnaire avec les attributs suivants:
    
    Id : un nombre aléatoire entre 1 et 10^6                                                                                                                                                           nom : le nom du questionnaire entrée par le professeur en haut de la page
    Id _utilisateurs : idProf qui a été récupéré depuis la page “prof.html”
    fileName : le nom du questionnaire entrée par le professeur en haut de la page
    
    Lorsque le professeur cliquera sur le bouton ”Sauvegarder le questionnaire”, je veux aussi que POUR CHAQUE types de questions sélectionnés via la checkbox, que tu m’ajoutes dans la table “questions” chaque question sélectionné par le professeur (une question sélectionné = une nouvelle entrée dans la table “questions”).
    Chaque entrée aura les caractéristiques suivantes :
    
    nom : le nom de la question qui a été coché avec la checkbox
    id _questionnaire : LE MÊME Id QUE LE QUESTIONNAIRE
    numero_question : 0
    infos_question : NULL
    
    NE CODE RIEN POUR L’INSTANT, je veux m’assurer que tu as compris ce que je te demande, pose moi également des questions supplémentaires si des aspects ne sont pas claires

--- index.js
+++ index.js
@@ -113,10 +113,10 @@ app.get('/api/questionnaires/:idProf', (req, res) => {
   const db = new sqlite3.Database(databaseName);
 
   const query = `
-    SELECT q.nom, q.fileName
-    FROM questionnaires AS q
-    INNER JOIN utilisateurs AS u ON q.Id_utilisateurs = u.Id
-    WHERE u.Id = ?;
+  SELECT q.nom, q.fileName
+  FROM questionnaires AS q
+  INNER JOIN utilisateurs AS u ON q.Id_utilisateurs = u.Id
+  WHERE u.Id = ?;
   `;
 
   db.all(query, [idProf], (err, rows) => {
@@ -127,6 +127,81 @@ app.get('/api/questionnaires/:idProf', (req, res) => {
       res.json(rows);
     }
   });
+
+  db.close();
+});
+
+
+//---------------------------------Gestion des questionnaires--------------------------------------------------------------
+app.get("/api/types_de_questions", (req, res) => {
+  const db = new sqlite3.Database(databaseName);
+
+  const query = "SELECT type_question FROM types_de_questions";
+
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Erreur lors de la récupération des types de questions");
+    } else {
+      res.json(rows);
+    }
+  });
+
+  db.close();
+});
+
+
+app.post("/api/save_questionnaire", (req, res) => {
+  const { idProf, nom, questions } = req.body;
+  const db = new sqlite3.Database(databaseName);
+  const idQuestionnaire = Math.floor(Math.random() * (10 ** 6));
+
+  const query = `
+    INSERT INTO questionnaires (Id, nom, Id_utilisateurs, fileName)
+    VALUES (?, ?, ?, ?);
+  `;
+
+  db.run(query, [idQuestionnaire, nom, idProf, nom], (err) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Erreur lors de la sauvegarde du questionnaire");
+    } else {
+      saveAllQuestions(db, questions, idQuestionnaire, 0, (err) => {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send("Erreur lors de la sauvegarde des questions");
+        } else {
+          res.status(200).send("Questionnaire et questions sauvegardés");
+        }
+      });
+    }
+  });
+
+  function saveQuestion(db, question, idQuestionnaire, callback) {
+    const { nom, numero_question } = question;
+    const query = `
+      INSERT INTO questions (nom, id_questionnaire, numero_question, infos_question)
+      VALUES (?, ?, ?, ?);
+    `;
+
+    db.run(query, [nom, idQuestionnaire, numero_question, null], (err) => {
+      callback(err);
+    });
+  }
+
+  function saveAllQuestions(db, questions, idQuestionnaire, index, finalCallback) {
+    if (index < questions.length) {
+      saveQuestion(db, questions[index], idQuestionnaire, (err) => {
+        if (err) {
+          finalCallback(err);
+        } else {
+          saveAllQuestions(db, questions, idQuestionnaire, index + 1, finalCallback);
+        }
+      });
+    } else {
+      finalCallback(null);
+    }
+  }
 
   db.close();
 });

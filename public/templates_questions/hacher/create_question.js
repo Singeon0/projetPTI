63361c188f4fd1b901fca41d6a8c9a2f9b08bf82
Alexandra Dache <alexia.dache@gmail.com>
2023-05-10 17:26:55 +0200

    hacher
    hacher questions

---
+++ public/templates_questions/hacher/create_question.js
@@ -0,0 +1,83 @@
+function parseQuestionnaireName() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const questionnaireName = urlParams.get("questionnaireName");
+  return questionnaireName;
+}
+
+document.getElementById("fraction_num").addEventListener("change", createPropositionsForm);
+document.getElementById("fraction_den").addEventListener("change", createPropositionsForm);
+document.getElementById("form").addEventListener("change", createPropositionsForm);
+document.getElementById("create-question-form").addEventListener("submit", createQuestionJSON);
+
+function createPropositionsForm() {
+  const numPropositions = document.getElementById("num-propositions").value;
+  const container = document.getElementById("propositions-container");
+  container.innerHTML = "";
+  
+  for (let i = 1; i <= numPropositions; i++) {
+    const labelLeft = document.createElement("label");
+    labelLeft.innerHTML = `Proposition gauche ${i} : `;
+    const inputLeft = document.createElement("input");
+    inputLeft.type = "text";
+    inputLeft.id = `proposition-left-${i}`;
+    
+    const labelRight = document.createElement("label");
+    labelRight.innerHTML = `Proposition droite ${i} : `;
+    const inputRight = document.createElement("input");
+    inputRight.type = "text";
+    inputRight.id = `proposition-right-${i}`;
+    
+    container.appendChild(labelLeft);
+    container.appendChild(inputLeft);
+    container.appendChild(labelRight);
+    container.appendChild(inputRight);
+  }
+}
+
+function saveQuestionToServer(questionJSON) {
+  fetch("/api/save_question", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(questionJSON)
+  }).then((response) => {
+    if (response.ok) {
+      alert("Question sauvegardée avec succès !");
+    } else {
+      alert("Erreur lors de la sauvegarde de la question.");
+    }
+  });
+}
+
+
+function createQuestionJSON(event) {
+  event.preventDefault();
+  
+  const questionnaireName = parseQuestionnaireName();
+
+  const questionName = document.getElementById("question-name").value;
+  const numPropositions = document.getElementById("num-propositions").value;
+  const propositionsLeft = [];
+  const propositionsRight = [];
+  const correctMatches = {};
+  
+  for (let i = 1; i <= numPropositions; i++) {
+    const propLeft = document.getElementById(`proposition-left-${i}`).value;
+    const propRight = document.getElementById(`proposition-right-${i}`).value;
+    propositionsLeft.push(propLeft);
+    propositionsRight.push(propRight);
+    correctMatches[propLeft] = propRight;
+  }
+
+  const questionJSON = {
+    titre_question: questionName,
+    propositions_gauche_ecran: propositionsLeft,
+    propositions_droite_ecran: propositionsRight,
+    correspondances_correctes: correctMatches,
+    questionnaireName: questionnaireName
+  };
+
+  saveQuestionToServer(questionJSON);
+}
+
